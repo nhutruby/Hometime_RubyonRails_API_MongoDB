1a851899dbe4c477ec306adab9d6525efd95e933
Benerik <benerik.nguyen@gmail.com>
2021-09-06 11:34:23 +0700

    refactor Reservation controller

--- app/controllers/concerns/payload.rb
+++ app/controllers/concerns/payload.rb
@@ -4,6 +4,29 @@
 module Payload
   extend ActiveSupport::Concern
 
+  def reservation_params(params)
+    if params[:payload][:reservation_code].present? && params[:payload][:guest] && params[:payload][:guest][:email].present?
+      checked_params = params.require(:payload).permit(:reservation_code, :start_date,
+                                                       :end_date, :nights, :guests,
+                                                       :adults, :children, :infants, :status,
+                                                       :currency, :payout_price, :security_price, :total_price,
+                                                       guest: [:first_name, :last_name, :phone, :email])
+      get_params_version_1(checked_params)
+    elsif params[:payload][:reservation] && params[:payload][:reservation][:code].present? && params[:payload][:reservation][:guest_email].present?
+      checked_params = params.require(:payload).permit(reservation: [:code, :start_date,
+                                                                     :end_date, :expected_payout_amount,
+                                                                     :guest_email, :guest_first_name, :guest_last_name,
+                                                                     :listing_security_price_accurate,
+                                                                     :host_currency, :nights, :number_of_guests,
+                                                                     :status_type, :total_paid_amount_accurate,
+                                                                     guest_details: [:localized_description,
+                                                                                     :number_of_adults, :number_of_children,
+                                                                                     :number_of_infants], guest_phone_numbers: []])
+      get_params_version_2(checked_params[:reservation])
+    else
+      {reservation: nil, guest: nil}
+    end
+  end
   def get_params_version_1(params)
     guest = {}
     guest[:first_name] = params[:guest][:first_name]
